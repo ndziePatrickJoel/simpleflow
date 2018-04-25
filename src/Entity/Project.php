5d31241f31c4af25b84386b46ece3cbe8bfb9ee0
NDZIE PATRICK <BMHB8456@adcm.orangecm>
2018-04-25 18:10:56 +0100

    Adding Conception files

---
+++ src/Entity/Project.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Entity;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Bucket
+ * a bucket contains many files and
+ * is associated with a table in database
+ * for each bucket a new table is created;
+ * @author BMHB8456
+ */
+class Project {
+    //put your code here
+        
+}
